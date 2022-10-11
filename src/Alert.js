272c464a2f6f44a0dd3469d886b2f26e23c635dc
harshita <harshita1225@gmail.com>
2022-10-11 13:53:06 +0200

    task completed

--- src/Alert.js
+++ src/Alert.js
@@ -1,12 +1,23 @@
-import React from "react"
-const Alert = props => {
-	//add the condition inside this function
+import React from "react";
+const Alert = (props) => {
+  //add the condition inside this function
 
-	return (
-		<div className="alert alert-danger" role="alert">
-			This is a primary alert-check it out!
-		</div>
-	);
+  /* return (
+    props.show && (
+      <div className="alert alert-danger" role="alert">
+        This is a primary alert-check it out!
+      </div>
+    ) 
+  );*/
+
+  if (!props.show) {
+    return null;
+  } else
+    return (
+      <div className="alert alert-danger" role="alert">
+        This is a primary alert-check it out!
+      </div>
+    );
 };
 
 export default Alert;
